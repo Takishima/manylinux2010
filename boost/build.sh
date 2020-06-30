b0373a9202078735d146b69a98788973d1734ee3
Damien Nguyen <damien1@huawei.com>
2020-06-30 10:14:27 +0200

    Automatic detection of installed Python versions

--- boost/build.sh
+++ boost/build.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+# ==============================================================================
+
+_pycall()
+{
+    PY_ROOT=$1
+    shift
+    PATH=$PY_ROOT/bin:$PATH "$@"
+}
+
+_pyversion()
+{
+    _pycall "$1" "$2" -c 'import sys; print("%i.%i" % (sys.version_info.major, sys.version_info.minor))'
+}
+
+# ==============================================================================
+
 echo 'source scl_source enable devtoolset-8' >> ~/.bashrc
 source scl_source enable devtoolset-8
 
@@ -9,10 +25,25 @@ yum install -y openmpi-devel gcc-c++ xz-devel bzip2-devel zlib-devel libicu-deve
 . /usr/share/Modules/init/sh
 module load openmpi-$(uname -i)
 
-/opt/python/cp35-cp35m/bin/python3 -m pip install numpy
-/opt/python/cp36-cp36m/bin/python3 -m pip install numpy
-/opt/python/cp37-cp37m/bin/python3 -m pip install numpy
-/opt/python/cp38-cp38/bin/python3  -m pip install numpy
+_tmp=""
+pyver_list=""
+for py_root in /opt/python/cp3*; do
+    _pycall "$py_root" python3 -m pip install numpy
+    echo $(_pycall "$py_root" python3 --version)
+    PY3_VERSION="$(_pyversion "$py_root" python3)"
+    pyver_list="${pyver_list},$PY3_VERSION"
+    abiflags="$(_pycall "$py_root" python3-config --abiflags)"
+    py_prefix="$(_pycall "$py_root" python3-config --prefix)"
+
+    _tmp="$_tmp\nusing python : ${PY3_VERSION:+$PY3_VERSION }: "
+    _tmp="$_tmp${PY3_VERSION:+${py_prefix}/bin/python3 }: "
+    _tmp="$_tmp${PY3_VERSION:+${py_prefix}/include/python${PY3_VERSION}${abiflags} }: "
+    _tmp="$_tmp${PY3_VERSION:+${py_prefix}/lib };"
+done
+python_user_config=$_tmp
+pyver_list=${pyver_list/,}
+
+sed -e "s/PYTHON_VERSION_LIST/$pyver_list/" -e "s|PYTHON_USER_CONFIG_JAM|$python_user_config|" SPECS/boost.spec.in > SPECS/boost.spec
 
 /bin/cp -vr . /root/rpmbuild
 
@@ -23,4 +54,8 @@ wget https://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_$
 
 echo cd /root/rpmbuild/SPECS
 cd /root/rpmbuild/SPECS
+
 rpmbuild --without mpich -ba boost.spec
+
+cd /root/rpmbuild/RPMS
+tar zcvf boost173_$(uname -p).tar.gz $(uname -p)/*
